cff8101542f9dddb87dd2426d48e3885239c0894
Sabyr <98071229+NurgaliyevS@users.noreply.github.com>
2022-04-24 12:08:00 +0600

    Fixed bug with double connection to Postgres

--- testing.py
+++ testing.py
@@ -1,56 +1,56 @@
-# import asyncio
-# from tabnanny import check
-# import asyncpg
-# from binance.client import Client
-# import os
-# import pandas as pd
-# from pycoingecko import CoinGeckoAPI
-# from aiohttp import web
-# import psycopg2
+import asyncio
+from tabnanny import check
+import asyncpg
+from binance.client import Client
+import os
+import pandas as pd
+from pycoingecko import CoinGeckoAPI
+from aiohttp import web
+import psycopg2
 
-# import settings
+import settings
 
-# cg = CoinGeckoAPI()
+cg = CoinGeckoAPI()
 
 
-# # async def main():
-# #     conn = await asyncpg.connect(f'postgresql://{settings.user}:{settings.password}@{settings.host}/{settings.db_name_2}')    
-# #     name = "bitcoin"
-# #     result = await conn.fetch("""
-# #     SELECT id, coin_name, coin_market_id
-# #     FROM coin
-# #     WHERE lower(coin_name) = $1::varchar
-# #     """, name)
-# #     print(result)
-# #     # Close the connection.
-# #     await conn.close()
-
-# async def get_coins_api_postgres():
-#     conn = await asyncpg.connect(f'postgresql://{settings.user}:{settings.password}@{settings.host}/{settings.db_name}')
-#     await conn.execute("TRUNCATE coins_info")
-#     page = 0    
-#     coin_market = cg.get_coins_markets(vs_currency='usd', per_page=250, page=page)
-#     df_market = pd.DataFrame(coin_market,columns=['market_cap_rank','id','name','current_price',"price_change_24h","price_change_percentage_24h",'market_cap',"market_cap_change_percentage_24h",'total_volume',  "circulating_supply", "max_supply", "high_24h", "low_24h", ])
-#     tuples = list(df_market.itertuples(index=False, name=None))
-#     await conn.copy_records_to_table('coins_info', records=tuples, columns=list(df_market), timeout=10)
+# async def main():
+#     conn = await asyncpg.connect(f'postgresql://{settings.user}:{settings.password}@{settings.host}/{settings.db_name_2}')    
+#     name = "bitcoin"
+#     result = await conn.fetch("""
+#     SELECT id, coin_name, coin_market_id
+#     FROM coin
+#     WHERE lower(coin_name) = $1::varchar
+#     """, name)
+#     print(result)
+#     # Close the connection.
 #     await conn.close()
 
-# async def check_crypto_price(coin):
-#     get_coins_api_postgres()
-#     conn = await asyncpg.connect(f'postgresql://{settings.user}:{settings.password}@{settings.host}/{settings.db_name}')
-#     res = await conn.fetch("""SELECT current_price
-# 	            FROM coins_info
-#                 WHERE lower(name) = $1::varchar OR id = $2::varchar;
-#                 """, coin, coin)
-#     await conn.close()
-#     strings = [str(integer) for integer in res]
-#     a_string = "".join(strings)
-#     a_string = a_string.replace('<', '').replace('>','').replace('Record ','')
-#     print(a_string)
-#     return a_string
+async def get_coins_api_postgres():
+    conn = await asyncpg.connect(f'postgresql://{settings.user}:{settings.password}@{settings.host}/{settings.db_name}')
+    await conn.execute("TRUNCATE coins_info")
+    page = 0    
+    coin_market = cg.get_coins_markets(vs_currency='usd', per_page=250, page=page)
+    df_market = pd.DataFrame(coin_market,columns=['market_cap_rank','id','name','current_price',"price_change_24h","price_change_percentage_24h",'market_cap',"market_cap_change_percentage_24h",'total_volume',  "circulating_supply", "max_supply", "high_24h", "low_24h", ])
+    tuples = list(df_market.itertuples(index=False, name=None))
+    await conn.copy_records_to_table('coins_info', records=tuples, columns=list(df_market), timeout=10)
+    await conn.close()
+
+async def check_crypto_price(coin):
+    get_coins_api_postgres()
+    conn = await asyncpg.connect(f'postgresql://{settings.user}:{settings.password}@{settings.host}/{settings.db_name}')
+    res = await conn.fetch("""SELECT current_price
+	            FROM coins_info
+                WHERE lower(name) = $1::varchar OR id = $2::varchar;
+                """, coin, coin)
+    await conn.close()
+    strings = [str(integer) for integer in res]
+    a_string = "".join(strings)
+    a_string = a_string.replace('<', '').replace('>','').replace('Record ','')
+    print(a_string)
+    return a_string
 
 # loop = asyncio.get_event_loop()
-# loop.run_until_complete(asyncio.gather(get_coins_api_postgres(), check_crypto_price('')))
+# loop.run_until_complete(asyncio.gather(get_coins_api_postgres(), check_crypto_price('bitcoin')))
 
 # def check_crypto_price(coin):
 #     try:
